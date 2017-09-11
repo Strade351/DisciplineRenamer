e71276fa94e70e070a79981a46781c871b6e9e2a
Strade351 <tolybondarev@gmail.com>
2017-09-11 17:32:00 +0400

    Note added

---
+++ src/Note.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Anatoliy on 11.09.2017.
+ */
+public class Note {
+    private String oldName;
+    private String newName;
+    Note(String oldName, String newName) {
+        this.oldName = oldName;
+        this.newName = newName;
+    }
+
+    public String getOldName() {
+        return oldName;
+    }
+
+    public void setOldName(String oldName) {
+        this.oldName = oldName;
+    }
+
+    public String getNewName() {
+        return newName;
+    }
+
+    public void setNewName(String newName) {
+        this.newName = newName;
+    }
+}
