8cca8ceaec58a782af3d82d74ebe0701b6d8f854
Strade351 <tolybondarev@gmail.com>
2017-09-11 19:24:18 +0400

    final

--- src/Note.java
+++ src/Note.java
@@ -4,6 +4,7 @@
 public class Note {
     private String oldName;
     private String newName;
+
     Note(String oldName, String newName) {
         this.oldName = oldName;
         this.newName = newName;
@@ -13,16 +14,8 @@ public class Note {
         return oldName;
     }
 
-    public void setOldName(String oldName) {
-        this.oldName = oldName;
-    }
-
     public String getNewName() {
         return newName;
-    }
-
-    public void setNewName(String newName) {
-        this.newName = newName;
     }
 
     @Override
